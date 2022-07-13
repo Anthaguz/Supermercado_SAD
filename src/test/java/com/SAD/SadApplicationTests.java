30ac468d918e12ad5569eff1145b1095d28b5a57
Anthaguz <anthonyag7@hotmail.com>
2022-07-12 22:31:55 -0600

    Inicializacion de repositorio

---
+++ src/test/java/com/SAD/SadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SAD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
