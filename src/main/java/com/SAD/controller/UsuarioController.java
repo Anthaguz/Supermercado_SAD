2d0aa739875cb007a818d0394ba58b2b245e36d0
Saul Hernandez <saulhdez28@gmail.com>
2022-08-07 01:29:32 -0600

    Agregar redirecciones corregidas + Bootstrap adjustments + Footer and Head para modulos actuales

--- src/main/java/com/SAD/controller/UsuarioController.java
+++ src/main/java/com/SAD/controller/UsuarioController.java
@@ -14,13 +14,11 @@ public class UsuarioController {
 
     @GetMapping("/usuario/informacion")
     public String mostrarInformacion(Model model) {
-
         return "/usuario/informacion";
     }
 
     @GetMapping("/usuario/contacto")
     public String mostrarContacto(Model model) {
-
         return "/usuario/contacto";
     }
 
