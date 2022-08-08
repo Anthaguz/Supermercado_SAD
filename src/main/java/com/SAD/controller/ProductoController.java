34602d8a790c9fd1caf17aa7a7dfc9039eb521b1
Anthaguz <anthonyag7@hotmail.com>
2022-08-08 11:32:04 -0600

    Carrito de compras, avance, aun no funciona pues necesito obtener el user context, saber quien esta logeado, pero hasta que no haya login con base de datos no podemos hacer mucho.

--- src/main/java/com/SAD/controller/ProductoController.java
+++ src/main/java/com/SAD/controller/ProductoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
@@ -20,11 +21,36 @@ public class ProductoController {
         var productos = productoService.getProductos(true);
         model.addAttribute("productos", productos);
         return "/producto/lista";
-    }    
-    @GetMapping("/producto/busqueda")
+    }
+
+    /*@GetMapping("/producto/busqueda")
     public String busqueda(Model model) {
         var productos = productoService.getProductos(true);
         model.addAttribute("productos", productos);
         return "/producto/lista";
+    }*/
+
+    @GetMapping("/producto/nuevo")
+    public String nuevoProducto(Producto producto, Model model) {
+        return "/producto/modificar";
+    }
+
+    @PostMapping("/producto/guardar")
+    public String guardarProducto(Producto producto) {
+        productoService.save(producto);
+        return "redirect:/producto/lista";
+    }
+
+    @GetMapping("/producto/modificar/{idProducto}")
+    public String modificarProducto(Producto producto, Model model) {
+        producto = productoService.getProducto(producto);
+        model.addAttribute("producto", producto);
+        return "/producto/modificar";
+    }
+
+    @GetMapping("/producto/eliminar/{idProducto}")
+    public String eliminarProducto(Producto producto) {
+        productoService.delete(producto);
+        return "redirect:/producto/lista";
     }
 }
