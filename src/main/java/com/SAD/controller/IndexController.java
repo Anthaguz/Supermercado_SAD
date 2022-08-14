c10ebe8aaa7b8e79be49722ad26fb7124b8a5419
Daniel Cascante <Dell@DESKTOP-6LRU2BN>
2022-08-14 02:37:55 -0600

    Commit Daniel Cascante security - Form completo, modificar los properties, ordenar informacion y empezando a crear la vista de inventario y separando productos

--- src/main/java/com/SAD/controller/IndexController.java
+++ src/main/java/com/SAD/controller/IndexController.java
@@ -1,18 +1,21 @@
 package com.SAD.controller;
 
+import com.SAD.service.ProductoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 @Slf4j
 public class IndexController {
-
+    @Autowired
+    private ProductoService productoService;
     @GetMapping("/")
     public String inicio(Model model) {
+        var productos = productoService.getProductos(true);
+        model.addAttribute("productos", productos);
         return "index";
 
     }
