1ae4fd7e8f7c412c0ad29ffc18c670291b20f89e
Anthaguz <anthonyag7@hotmail.com>
2022-08-14 22:02:16 -0600

    Login completely functional

--- src/main/java/com/SAD/domain/Usuario.java
+++ src/main/java/com/SAD/domain/Usuario.java
@@ -6,47 +6,21 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name="Usuario")
-public class Usuario implements Serializable{
-    private static final long serialVersionUID=1L;
+@Table(name="usuario")
+public class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private long idUsuario;
-    private String nombre;
-    private String apellido;
-    private String correo;
-    private String direccion;
-    private int telefono;
-    Long idCliente;
-    Long idRol;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario; // Hibernate lo transforma en id_cliente    
     String username;
     String password;
-
-    public Usuario(long idUsuario, String nombre, String apellido, String correo, String direccion, int telefono, Long idCliente, Long idRol, String username, String password) {
-        this.idUsuario = idUsuario;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.correo = correo;
-        this.direccion = direccion;
-        this.telefono = telefono;
-        this.idCliente = idCliente;
-        this.idRol = idRol;
-        this.username = username;
-        this.password = password;
-    }
-
-
-
+    Long idCliente;
+    Long idRol;
     public Usuario() {
     }
-
-    public Usuario(String nombre, String apellido, String correo, String direccion, int telefono) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.correo = correo;
-        this.direccion = direccion;
-        this.telefono = telefono;
+    public Usuario(String username, String password, long idRol) {
+        this.username = username;
+        this.password = password;
+        this.idRol = idRol;
     }
-    
-    
 }
