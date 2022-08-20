7d22840a2b5557be086b41c969b479840a01f4dd
Daniel Cascante <Dell@DESKTOP-6LRU2BN>
2022-08-19 22:00:19 -0600

    Commit Daniel Cascante JasperReport de el inventario, modificar el "Agregar" producto

--- src/main/java/com/SAD/controller/ProductoController.java
+++ src/main/java/com/SAD/controller/ProductoController.java
@@ -1,18 +1,27 @@
 package com.SAD.controller;
 
 import com.SAD.domain.Producto;
+import com.SAD.service.FacturaReportService;
 import com.SAD.service.ProductoService;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Slf4j
 public class ProductoController {
 
+    @Autowired
+    private FacturaReportService facturaReportService;
     @Autowired
     private ProductoService productoService;
 
@@ -29,7 +38,6 @@ public class ProductoController {
         model.addAttribute("productos", productos);
         return "/producto/lista";
     }*/
-
     @GetMapping("/producto/nuevo")
     public String nuevoProducto(Producto producto, Model model) {
         return "/producto/modificar";
@@ -52,5 +60,23 @@ public class ProductoController {
     public String eliminarProducto(Producto producto) {
         productoService.delete(producto);
         return "redirect:/producto/lista";
+    }
+
+    @GetMapping(value = "/producto/Factura", produces = MediaType.APPLICATION_PDF_VALUE)
+    public @ResponseBody
+    byte[] getFile() throws IOException {
+        try {
+            FileInputStream fis = new FileInputStream(new File(facturaReportService.generateReport()));
+            byte[] targetArray = new byte[fis.available()];
+            fis.read(targetArray);
+            return targetArray;
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
     }
 }
